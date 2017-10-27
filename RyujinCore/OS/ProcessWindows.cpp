bca05a694313de8789c885fd2dbc3c6631531f44
Xavi <yggdrasil.917@gmail.com>
2017-10-27 01:07:39 -0400

    Updates to windows process

--- RyujinCore/OS/ProcessWindows.cpp
+++ RyujinCore/OS/ProcessWindows.cpp
@@ -1,10 +1,30 @@
 #include "Process.hpp"
-
+#include "../Containers/String.hpp"
 
 namespace Ryujin
 {
 	void Process::RunProcess(const char* pathToExecutable, const char* arguments, bool bCaptureOutput, bool bWait /* = true */)
 	{
+		STARTUPINFO si;
+		PROCESS_INFORMATION pi;
+		char* args = new char[strlen(arguments)+1];
+		strcpy(args, arguments);
 
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		ZeroMemory(&pi, sizeof(pi));
+		if (!CreateProcess(
+				pathToExecutable,
+				args, NULL, NULL, FALSE,
+				NULL,
+				NULL, NULL,
+				&si,
+				&pi)
+			)
+		{
+			delete args;
+			return;
+		}
+		delete args;
 	}
 }
